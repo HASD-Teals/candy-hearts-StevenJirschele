5ab63cf6c0e8b8fe09026d66939c22fa327bf446
StevenJirschele <bmaxx321@gmail.com>
2022-01-24 09:55:58 -0600

    Done for now

--- src/Heart.java
+++ src/Heart.java
@@ -2,30 +2,96 @@ public class Heart {
     // PROPERTIES
     private String color;
     //Add remaining heart properties here...
-
+    private String sayings;
+    private int size;
+    private int calories;
+    private int available;
+    private double costPerPound;
+    private boolean stock;
 
     // CONSTRUCTORS
-    public Heart() {} // This is the default contructor
+    public Heart() {
+    this.color = "";
+    this.sayings = "";
+    this.size = 0;
+    this.calories = 0;
+    this.available = 0;
+    this.costPerPound = 0;
+    this.stock = false;
+    } 
+    public Heart(String color, String sayings, int calories, int available, double costPerPound, boolean stock) {
+        // This is the default contructor
     //Add overloaded Constructor here...
-
-
+    this.color = color;
+    this.sayings = sayings;
+    this.calories = calories;
+    this.available = available;
+    this.costPerPound = costPerPound;
+    this.stock = stock;
+}
     // ACCESSORS
     public String getColor() {
         return this.color;
     }
-    //Add remaining Accessor methods here...
+    public String getSaying() {
+        return this.sayings;
+    }
+    public int size() {
+        return this.size;
+    }
+    public int getCalories() {
+        return this.calories;
+    }
+    public int getAvailable() {
+        return this.available;
+    }
+    public double getCostPerPound() {
+        return this.costPerPound;
+    }
+    public boolean getStock() {
+        return this.stock;
+    }
 
+    //Add remaining Accessor methods here...
 
     // MUTATORS
     public void setColor(String color) {
         this.color = color;
     }
     //Add remaining Mutator methods here...
-
+    public void setSayings(String sayings) {
+        this.sayings = sayings;
+    }
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public void setGetCalories(int calories) {
+        this.calories = calories;
+    }
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+    public void setCostPerPound(int costPerPound) {
+        this.costPerPound = costPerPound;
+    }
+    public void setStock(boolean stock) {
+        this.stock = stock;
+    }
 
     // METHODS
     public String report() {
-        return this.color + "\n"; // Add other properties here as you add them to the class
+        return this.color + "\n" + this.available + "\n" + this.stock; // Add other properties here as you add them to the class
     }
     //Write other methods here (if needed)...
+    public double adjPrice(double percentage) {
+        return this.costPerPound = costPerPound * percentage;
+    }
+    public boolean inStock(int numHearts) {
+        if(numHearts > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- src/HeartsClient.java
+++ src/HeartsClient.java
@@ -1,5 +1,8 @@
 public class HeartsClient {
     public static void main(String[] args) throws Exception {
-        
+      Heart Group1 = new Heart("red", 3, 5, "Hi", 5.00, 50);
+      Heart Group2 = new Heart();
+      Heart Group3 = new Heart();  
     }
+
 }
